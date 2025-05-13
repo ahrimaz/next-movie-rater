25d8a059313e7e6958cbec776168439a67fffb70
ahrimaz <eric.stanard@gmail.com>
2025-05-13 17:52:00 -0400

    removed unused session definition

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { signIn, useSession } from "next-auth/react";
+import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 
